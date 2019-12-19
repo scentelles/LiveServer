f447d5c37c8ba13ff4524cadd681bf49c2ebeaa0
Sylvain Centelles <scentellesgmail.com>
2019-12-19 23:08:44 +0100

    modify startup to add launch of OSC 2 BLE bridge

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 /usr/bin/qlcplus -o /home/pi/Projects/LiveServer/QLC_setup.qxw -p &
 
 sleep 10
+echo "Launching LiveServer"
+/usr/bin/lxterm -title "LiveServer" -hold -e '/usr/bin/python3 /home/pi/Projects/LiveServer/LiveServer.py'
 
-/usr/bin/python3 /home/pi/Projects/LiveServer/LiveServer.py
+#echo "Launching OSC 2 BLE bridge"
+#/usr/bin/lxterm -title "OSC2BLE" -hold -e '/usr/bin/python3 /home/pi/Projects/LiveServer/OSC2BLE.py'&
+
