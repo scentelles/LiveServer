9056ac5c0c7a7db92fe561fbffc5cde1ac641dc1
Sylvain Centelles <scentelles@gmail.com>
2021-10-23 23:02:27 +0200

    add raspebbry scripts to simulation mode

--- VideoPC/startup - simulator.bat
+++ VideoPC/startup - simulator.bat
@@ -1,9 +1,14 @@
 ::start projectM
 ::start Musicbeam
 
-start "Simulator" python -u ..\Simulator\simulator.py
+::start "Simulator" python -u ..\Simulator\simulator.py
+start "VoiceLiveSimu" python -u ..\Simulator\voicelive_simulator.py
+timeout /t 1
+start /min "Video Manager" python -u videoManager.py config.json
 timeout /t 2
-start "Video Manager" python -u videoManager.py config.json
+start /min "LiveServer" python -u ..\LiveServer.py simulation_mode
+timeout /t 2
+start /min "OSC2VIDEO" python -u ..\OSC2BLE.py simulation_mode
 
 pushd .
 
@@ -11,7 +16,7 @@ timeout /t 2
 ::Path for obs config files : %appdata%/obs-studio
 ::https://obsproject.com/wiki/Launch-Parameters
 cd %cd%\visualizer
-start projectM.exe
+start /min projectM.exe
 cd "C:\Program Files\obs-studio\bin\64bit" 
 start obs64.exe --profile "Smooth" --collection "Smooth" --scene "Default" --allow-opengl --minimize-to-tray
 
