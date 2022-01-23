92386854a345793bec819b3ca1d9c5204425cf58
Sylvain Centelles <scentelles@gmail.com>
2022-01-23 15:41:34 +0100

    remove LH scripts, as non reliable solution

--- setLHModeDmx.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-from pythonosc import udp_client
-clientLH = udp_client.SimpleUDPClient("10.3.141.90", 8001)
-clientLH.send_message("/laserharp/startDMX", 0)
--- setLHModeLH.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-
-LHPreset = sys.argv[1]
-print (LHPreset)
-
-from pythonosc import udp_client
-clientLH = udp_client.SimpleUDPClient("10.3.141.90", 8001)
-clientLH.send_message("/laserharp/startLH", int(LHPreset))
