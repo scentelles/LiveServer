7dbe0670285d2bbe6fd7d81a5e5a8a737259ef82
Sylvain Centelles <scentelles@gmail.com>
2022-02-02 21:55:07 +0100

    update startup with cantabile and add new script to close all

--- VideoPC/startup - surface simu.bat
+++ VideoPC/startup - surface simu.bat
@@ -3,9 +3,14 @@
 
 ::start "transmidifier" /min "C:\Program Files (x86)\TransMIDIfier\TransMIDIfier.exe"
 ::timeout /t 1
-start "Midi2OSC" python -u ..\WinPC\Midi2OSC.py
+
+
+
+
+start "Cantabile" "C:\Program Files\Topten Software\Cantabile 3.0\Cantabile.exe"
+start /min "Midi2OSC" python -u ..\WinPC\Midi2OSC.py
 timeout /t 1
-start "OSC_QLC DISPATCH" python -u ..\OSC_qlc_out_dispatcher.py
+start /min "OSC_QLC_DISPATCH" python -u ..\OSC_qlc_out_dispatcher.py
 ::timeout /t 1
 ::start "Kontakt" "C:\Program Files\Native Instruments\Kontakt\Kontakt.exe"
 ::timeout /t 1
@@ -13,13 +18,13 @@ start "OSC_QLC DISPATCH" python -u ..\OSC_qlc_out_dispatcher.py
 start C:\QLC+\qlcplus.exe -p -o ..\QLC_setup.qxw
 timeout /t 1
 
-start "VoiceLiveSimu" python -u ..\Simulator\voicelive_simulator.py simulation_mode
+start /min "VoiceLiveSimu" python -u ..\Simulator\voicelive_simulator.py simulation_mode
 ::timeout /t 1
-start /min "Video Manager" python -u videoManager.py config.json
+start /min "VideoManager" python -u videoManager.py config.json
 timeout /t 2
 start /min "LiveServer" python -u ..\LiveServerWindows.py 
 ::timeout /t 2
-start /min "KeyressManager" python -u ..\keypressManager.py 
+start /min "KeypressManager" python -u ..\keypressManager.py 
 ::timeout /t 2
 pushd .
 
@@ -27,8 +32,8 @@ timeout /t 2
 ::Path for obs config files : %appdata%/obs-studio
 ::https://obsproject.com/wiki/Launch-Parameters
 cd %cd%\visualizer
-start /min projectM.exe
+start "projectM" /min projectM.exe
 cd "C:\Program Files\obs-studio\bin\64bit" 
-start obs64.exe --profile "Smooth" --collection "Smooth" --scene "Default" --allow-opengl --minimize-to-tray
+start "OBS" obs64.exe --profile "Smooth" --collection "Smooth" --scene "Default" --allow-opengl --minimize-to-tray
 
 popd
