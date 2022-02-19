ada62c07a528a9a70bf5afe44d538e8d59962cf9
Sylvain Centelles <scentelles@gmail.com>
2022-02-19 10:39:52 +0100

    Update to deal only with OSC to BLE bridge, as per archi v2 mode

--- OSC2BLE.py
+++ OSC2BLE.py
@@ -16,16 +16,9 @@ from pythonosc import udp_client
 
 from SmoothDefines import *  #TODO : used only for OBS. not smooth specific
 
-print ("Argument nb:", len(sys.argv))
-
-if(len(sys.argv) > 1):
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    videoPC_ip = local_ip
-else:
-    from bluepy import btle
-    #local_ip = "10.3.141.1"
-    videoPC_ip = "10.3.141.52"
+from bluepy import btle
+#local_ip = "10.3.141.1"
+videoPC_ip = "10.3.141.52"
 
 
 ledThreads = {}
@@ -35,25 +28,13 @@ MAC_ADDRESS_GUI   = "ff:ff:bc:00:2b:78"
 MAC_ADDRESS_LED4  = "ff:ff:ac:00:24:1b"
 MAC_ADDRESS_TEST  = "ff:ff:10:0f:51:dc"
 
-OSC_ADDRESS   = "127.0.0.1"
-OSC_PORT      = 9002         #corresponds to universe #3
-
-DMX_CHANNEL_LH_CONTROL = 16
-LH_CONTROL_IDLE = 1
-LH_CONTROL_DMX  = 2
-LH_CONTROL_LH_PRESET_OFFSET  = 3
-
-
+OSC_ADDRESS   = "10.3.141.2"
+OSC_PORT      = 9003         #corresponds to universe #4
 
 
 DEBOUNCE_DELAY = 0.3
 DELAY_TO_SEND_LAST_VALUES = 0.6
 
-
-clientLH = udp_client.SimpleUDPClient("10.3.141.90", 8001)
-clientMS = udp_client.SimpleUDPClient("10.3.141.133", 7701)
-#OSC connection to Video PC
-clientVideoPC = udp_client.SimpleUDPClient(videoPC_ip, 5007)
 
 def dispatch_qlc_osc_handler(osc_address, args, command):
 
@@ -65,59 +46,31 @@ def dispatch_qlc_osc_handler(osc_address, args, command):
   channelIndex = int(channel)%3
   value = round(command*255)
 
-  #Channels >= 15 => OSC controls of Megascreen, VideoPC, etc...
-  if(int(channel) >= 15):
-    if(int(channel) == DMX_CHANNEL_LH_CONTROL - 1):
-      print("Received LH OSC Command")
+  myLed = ledThreads[ledIndex]
 
-      if(value == LH_CONTROL_IDLE):
-        print("LH : Setting LH in IDLE mode")
-        clientLH.send_message("/laserharp/startIDLE", 0)
-      if(value == LH_CONTROL_DMX):
-        print("LH : Settinh LH in DMX mode")
-        clientLH.send_message("/laserharp/startDMX", 0)
-      if(value >= LH_CONTROL_LH_PRESET_OFFSET):
-        preset = value - LH_CONTROL_LH_PRESET_OFFSET
-        print("LH : Setting LH mode, preset : " + str(preset))
-        clientLH.send_message("/laserharp/startLH", preset)      #To be tested
-      return
-    
-    process_OSC_2_MegaScreen(clientMS, channel, value)
-    
-    process_OSC_2_video_PC(clientVideoPC, channel, value)
- 
+  if (myLed.connected):
+           #   print(str(myLed.name) + " : " + str(value))
+              if(channelIndex == 0):
+                 myLed.R = value
+              if(channelIndex == 1):
+                 myLed.G = value
+              if(channelIndex == 2):
+                 myLed.B = value
 
-  
-  #print("address" + osc_address + " : " + str(ledIndex) + " : " + str(channelIndex) + " : " + str(value))
-  
-  #For channes < 15, BLE controls
-  else: #for DMX channel between 0 and 15)
-    myLed = ledThreads[ledIndex]
-
-    if (myLed.connected):
-             #   print(str(myLed.name) + " : " + str(value))
-                if(channelIndex == 0):
-                   myLed.R = value
-                if(channelIndex == 1):
-                   myLed.G = value
-                if(channelIndex == 2):
-                   myLed.B = value
-
-                #TODO: FIX. workaround because too many messages, losing some values.
-               # if(myLed.R + myLed.R + myLed.R <60) :
-                #    myLed.R = 0
-                 #   myLed.G = 0
-                  #  myLed.B = 0
-                try:	               
-                  myLed.bleWrite()
-                  #time.sleep(0.2) #change debug
-                except Exception as e:
-                  print('Exception : Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
-                  print("DISCONNECTED!!!!!!!")
-                  myLed.connected = 0
-                  myLed.connect()
-
-    else:
+              #TODO: FIX. workaround because too many messages, losing some values.
+             # if(myLed.R + myLed.R + myLed.R <60) :
+              #    myLed.R = 0
+               #   myLed.G = 0
+                #  myLed.B = 0
+              try:	               
+                myLed.bleWrite()
+                #time.sleep(0.2) #change debug
+              except Exception as e:
+                print('Exception : Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+                print("DISCONNECTED!!!!!!!")
+                myLed.connected = 0
+                myLed.connect()
+  else:
       print("Received command, but still not connected to " + str(myLed.name) + ". Trying to connect now")
       myLed.connect()
 
