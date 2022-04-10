b80f8653c04e4a7dee5261d90f11e25219daf4f9
Sylvain Centelles <scentelles@gmail.com>
2022-04-10 21:30:15 +0200

    update startup sript and scp push

--- pushScriptToRaspi.sh
+++ pushScriptToRaspi.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-scp LiveServer.py pi@10.3.141.2:/home/pi/Projects/LiveServer/LiveServer.py
-scp OSC2BLE.py pi@10.3.141.2:/home/pi/Projects/LiveServer/OSC2BLE.py
+#scp LiveServer.py pi@10.3.141.2:/home/pi/Projects/LiveServer/LiveServer.py
+#scp OSC2BLE.py pi@10.3.141.2:/home/pi/Projects/LiveServer/OSC2BLE.py
 scp QLC_setup-vindhelfest.qxw pi@10.3.141.2:/home/pi/Projects/LiveServer/QLC_setup-vindhelfest.qxw
--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
-/usr/bin/qlcplus -o /home/pi/Projects/LiveServer/QLC_setup-raspi.qxw -p &
+/usr/bin/lxterm -title "LCD" -hold -e 'sudo /usr/bin/python3 /home/pi/Projects/LiveServer/LCD.py' &
+
+/usr/bin/lxterm -title "LiveServer" -hold -e  '/usr/bin/qlcplus -o /home/pi/Projects/LiveServer/QLC_setup-raspi.qxw -p' &
 
 sleep 15
 echo "Launching LiveServer"
-/usr/bin/lxterm -title "LiveServer" -hold -e '/usr/bin/python3 /home/pi/Projects/LiveServer/LiveServer.py'
+
+/usr/bin/lxterm -title "LiveServer" -hold -e '/usr/bin/python3 /home/pi/Projects/LiveServer/LiveServer.py' &
 
 #echo "Launching OSC 2 BLE bridge"
 #/usr/bin/lxterm -title "OSC2BLE" -hold -e '/usr/bin/python3 /home/pi/Projects/LiveServer/OSC2BLE.py'&
