c77d579ae7fe93f9040b16c4cc6ddc49b09e5852
Sylvain Centelles <scentelles@gmail.com>
2023-05-29 21:55:22 +0200

    launch vhf QLC setup by default

--- startup.sh
+++ startup.sh
@@ -1,11 +1,15 @@
 /usr/bin/lxterm -title "LCD" -hold -e 'sudo /usr/bin/python3 /home/pi/Projects/LiveServer/LCD.py' &
 
-/usr/bin/lxterm -title "QLC" -hold -e  '/usr/bin/qlcplus -o /home/pi/Projects/LiveServer/QLC_setup-PJVMusical.qxw -p' &
 
-sleep 30
+/usr/bin/lxterm -title "QLC" -hold -e  '/usr/bin/qlcplus -o /home/pi/Projects/LiveServer/QLC_setup-vindhelfest.qxw -p' &
+
+sleep 20
+#/usr/bin/lxterm -title "LiveServer" -hold -e '/usr/bin/python3 -u /home/pi/Projects/LiveServer/LiveServer.py >> /home/pi/Projects/LiveServer/startup.log' &
+
+
+
 echo "Launching LiveServer"
-
-/usr/bin/lxterm -title "LiveServer" -hold -e '/usr/bin/python3 /home/pi/Projects/LiveServer/LiveServer.py' &
+/usr/bin/python3 -u /home/pi/Projects/LiveServer/LiveServer.py
 
 #echo "Launching OSC 2 BLE bridge"
 #/usr/bin/lxterm -title "OSC2BLE" -hold -e '/usr/bin/python3 /home/pi/Projects/LiveServer/OSC2BLE.py'&
