3540790b9b99f9680cf602a0e0ec1278810f1ec3
Sylvain Centelles <scentelles@gmail.com>
2024-05-05 11:51:02 +0200

    before prep VHF3

--- pullConfigFromRaspi.sh
+++ pullConfigFromRaspi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-scp pi@10.3.141.2:/home/pi/Projects/LiveServer/QLC_setup-raspi.qxw QLC_setup-raspi.qxw
+scp pi@10.3.141.2:/home/pi/Projects/LiveServer/QLC_setup-vindhelfest.qxw QLC_setup-vindhelfest.qxw
 scp pi@10.3.141.2:/home/pi/Projects/LiveServer/startup.sh startup.sh
